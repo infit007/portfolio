27ab4ac341b0c8c1d4a65ebdb547a7ba43e869fa
Jayesh Suntha <wjsuntha@gmail.com>
2025-01-23 13:21:57 +0530

    commit

---
+++ src/components/Skills.tsx
@@ -0,0 +1,26 @@
+import {
+    Card,
+    CardHeader,
+    CardTitle,
+    CardContent
+} from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+// TODO: Update skills
+const skills = ["TypeScript", "JavaScript", "Python", "Java", "C#", "C++", "C", "SQL", "HTML", "CSS", "TailwindCSS", "NextJS", "ReactJS", "NodeJS", "ExpressJS", "MongoDB", "PostgreSQL", "Git", "Docker", "Kubernetes", "Linux", "Windows", "MacOS"]
+
+export const Skills = () => {
+    return (
+        <Card className="mt-6">
+            <CardHeader>
+                <CardTitle>Skills</CardTitle>
+            </CardHeader>
+            <CardContent>
+                <div className="flex flex-wrap gap-2">
+                    {skills.map((s, i) => (
+                        <Badge key={i} variant="secondary">{s}</Badge>
+                    ))}
+                </div>
+            </CardContent>
+        </Card>
+    )
+}
